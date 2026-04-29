c23d0b37e4a883312fabceeaa4f0f04eec4930e7
James Todd <jtodd0515@gmail.com>
2026-04-29 00:27:31 -0400

    fix contact page whiteout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import Image from "next/image";
-import dynamic from 'next/dynamic';
 
-const WindowSizeDisplay = dynamic(() => import('@/components/WindowSizeDisplay'), { ssr: false });
 
 export default function Home() {
   return (
-    <div>
-    <main>
-      <WindowSizeDisplay />
-      {/* ... */}
-    </main>
+    
 
       <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
           <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -41,6 +35,5 @@ export default function Home() {
             </div>
           </main>
         </div>
-    </div>
   );
 }
--- components/WindowSizeDisplay.js
+++
@@ -1,7 +0,0 @@
-'use client'
-import { useWindowSize } from '@/components/WindowSize'
-
-export default function WindowSizeDisplay() {
-  const { width, height } = useWindowSize()
-  return <div>Window: {width} x {height}</div>
-}
